0362833c544658698e8eb681f373c47f75c0d2d8
Leon De Silva <leon.desilva>
2021-12-02 23:22:42 +0530

    Added the implementation of the product service with connectivity to database

--- products-service/src/main/java/com/leondesilva/devopsassignment/productsservice/ProductsServiceApplication.java
+++ products-service/src/main/java/com/leondesilva/devopsassignment/productsservice/ProductsServiceApplication.java
@@ -3,11 +3,18 @@ package com.leondesilva.devopsassignment.productsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application class
+ */
 @SpringBootApplication
 public class ProductsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsServiceApplication.class, args);
-	}
-
+    /**
+     * Main method of the application.
+     *
+     * @param args the arguments to pass
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsServiceApplication.class, args);
+    }
 }
